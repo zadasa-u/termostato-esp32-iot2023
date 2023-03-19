f5d5dd106c79fc1bc44d8dff72b1c604fb4377cb
zadasa-u <danielzakowicz007@gmail.com>
2023-03-18 23:15:43 -0300

    Primera version

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # termostato-esp32-iot2023
+
+Crear un termostato con un esp32 que puede funcionar de modo manual o automático y que cumpla con las siguientes especificaciones:
+
+- Programado en micropython utilizando asyncio
+- sensor dht22.
+- se comunica mediante mqtts.
+- publica periódicamente en el tópico "ID del dispositivo" los siguientes parámetros: temperatura, humedad, setpoint, periodo y modo.
+- se envía en una sola publicación en un JSON
+- se suscriba a setpoint, periodo, destello, modo y relé.
+- almacenará de manera no volátil los parámetros de setpoint, periodo, modo y relé (ver btree).
+- contará con relé que se accionará cuando se supere la temperatura de setpoint. (modo automático)
+- si se encuentra en modo manual el relé se activará según la orden "relé" enviada por mqtt.
+- destellará por unos segundos cuando reciba la orden "destello" por mqtt.
+- cuando recibe un mensaje con nuevos parámetros deberá actualizar los almacenados y actuar si es necesario.
+- el código del programa deberá estar en un repositorio git cuya dirección debe figurar en la entrega del trabajo.
---
+++ main.py
@@ -0,0 +1,95 @@
+from mqtt_as import MQTTClient
+from mqtt_local import config
+import uasyncio as asyncio
+import dht, machine
+
+# Configuracion de pines:
+DHT = 13
+RELE = 12
+LED = 27
+
+# sensor
+d = dht.DHT22(machine.Pin(DHT))
+
+# rele:
+rele = machine.Pin(RELE,machine.Pin.OUT)
+
+# led para destellos:
+led = machine.Pin(LED,machine.Pin.OUT)
+
+# Constantes y etiquetas:
+SPT = 30 # temperatura umbral predeterminada
+PER = 20 # periodo de publicacion predeterminado
+MOD = 'AUTO' # modo de funcionamiento predeterminado
+
+D = 0.5 # tiempo de destello del led
+
+ID = config['client_id']
+
+# defino cadenas para subtopicos:
+TT = '{}/temperatura'.format(ID)
+TH = '{}/humedad'.format(ID)
+TS = '{}/setpoint'.format(ID)
+TP = '{}/periodo'.format(ID)
+TM = '{}/modo'.format(ID)
+
+###########################################################
+# funciones para configuracion del cliente:
+def sub_cb(topic, msg, retained):
+    print('Topic = {} -> Valor = {}'.format(topic.decode(), msg.decode()))
+
+async def wifi_han(state):
+    print('Wifi is ', 'up' if state else 'down')
+    await asyncio.sleep(1)
+
+async def conn_han(client):
+    # topicos que el mismo cliente publica
+    #await client.subscribe(TT, 1)
+    #await client.subscribe(TH, 1)
+
+    # topicos para comando remoto del termostato:
+    await client.subscribe('setpoint', 1)
+    await client.subscribe('periodo', 1)
+    await client.subscribe('modo', 1)
+    
+    await client.subscribe('destello', 1)
+    await client.subscribe('rele', 1)
+
+# Define configuracion del cliente
+config['subs_cb'] = sub_cb
+config['connect_coro'] = conn_han
+config['wifi_coro'] = wifi_han
+config['ssl'] = True
+
+# Inicia cliente
+MQTTClient.DEBUG = True
+client = MQTTClient(config)
+
+###########################################################
+# funciones generales
+async def main(client):
+    await client.connect()
+    
+    await asyncio.sleep(2)
+    while True:
+        try:
+            d.measure()
+            try:
+                temperatura=d.temperature()
+                await client.publish(TT, '{}'.format(temperatura), qos = 1)
+            except OSError as e:
+                print("sin sensor temperatura")
+            try:
+                humedad=d.humidity()
+                await client.publish(TH, '{}'.format(humedad), qos = 1)
+            except OSError as e:
+                print("sin sensor humedad")
+        except OSError as e:
+            print("sin sensor")
+        await asyncio.sleep(20)
+
+try:
+    asyncio.run(main(client))
+finally:
+    client.close()
+    asyncio.new_event_loop()
